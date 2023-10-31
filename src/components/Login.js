97500bedfead0334d28920ec80de34c861f25ac3
Meriem <meriemlaieb225@gmail.com>
2023-10-31 11:19:55 +0100

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hi</h1>
+        <h1>hello</h1>
+      </div>
+    )
+  }
+}
+
+export default Login
